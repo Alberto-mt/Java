0a292d0d1cecb051cfa3df5b7fa7d72e3b77b67a
Alberto Montealegre <alberto_mt@hotmail.es>
2023-06-10 19:25:23 +0200

    POO_MVC_Ejercicio2/src/controlador/Ejecutador

---
+++ POO_MVC_Ejercicio2/src/controlador/Ejecutador.java
@@ -0,0 +1,11 @@
+package controlador;
+
+public class Ejecutador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gestiona ges = new Gestiona();
+		ges.programa();
+	}
+
+}
