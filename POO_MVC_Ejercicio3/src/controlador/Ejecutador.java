9dd1a76efafe964099282078a4b088a4e6a9561d
Alberto Montealegre <alberto_mt@hotmail.es>
2023-06-15 12:24:43 +0200

    POO_MVC_Ejercicio3/src/controlador/Ejecutador

---
+++ POO_MVC_Ejercicio3/src/controlador/Ejecutador.java
@@ -0,0 +1,26 @@
+package controlador;
+
+import vista.AreaDatos;
+import vista.ComboDatos;
+import vista.ListaDatos;
+import vista.TablaDatos;
+
+public class Ejecutador {
+
+	public static void main(String[] args) {
+		Gestiona ges = new Gestiona();
+		ges.programa();
+		
+		TablaDatos tabla = new TablaDatos();
+		tabla.ejecutador(tabla);
+		
+		ListaDatos lista = new ListaDatos();
+		lista.ejecutador(lista);
+		
+		ComboDatos combo = new ComboDatos();
+		combo.ejecutador(combo);
+		
+		AreaDatos area = new AreaDatos();
+		area.ejecutador(area);
+	}
+}
